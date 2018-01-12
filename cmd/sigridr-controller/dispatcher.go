7d9cefa342a51a58b457e91a315c541fa96fcdc7
Marius André Elsfjordstrand Beck <marius.beck@nb.no>
2018-01-12 15:20:57 +0100

    Do not quit when failing to dial agent, just log error

--- cmd/sigridr-controller/dispatcher.go
+++ cmd/sigridr-controller/dispatcher.go
@@ -5,9 +5,10 @@ import (
 
 	"google.golang.org/grpc"
 	"golang.org/x/net/context"
+	log "github.com/sirupsen/logrus"
+
 	"github.com/nlnwa/sigridr/api/sigridr"
 	"github.com/nlnwa/sigridr/pkg/types"
-	log "github.com/sirupsen/logrus"
 )
 
 func dispatch(job *types.Job, seed *types.Seed) {
@@ -16,12 +17,10 @@ func dispatch(job *types.Job, seed *types.Seed) {
 		Seed: seed.ToProto(),
 	}
 
-	log.WithField("request", request).Debugln("Dispatch request to agent")
-
 	opts := grpc.WithInsecure()
 	conn, err := grpc.Dial(address, opts)
 	if err != nil {
-		log.WithError(err).Fatal()
+		log.WithError(err).Error()
 	}
 	defer conn.Close()
 
@@ -33,5 +32,7 @@ func dispatch(job *types.Job, seed *types.Seed) {
 	_, err = agent.Do(ctx, &request)
 	if err != nil {
 		log.WithError(err).Error()
+	} else {
+		log.WithField("seed", seed.Meta.Description).Infoln("Dispatch")
 	}
 }
