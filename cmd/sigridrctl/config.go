981c86bd4599992807718ead724077a02ccb77b5
Marius André Elsfjordstrand Beck <marius.beck@nb.no>
2018-01-23 13:49:59 +0100

    Remove logrus dependency and add error handling when writing config

--- cmd/sigridrctl/config.go
+++ cmd/sigridrctl/config.go
@@ -1,10 +1,24 @@
+// Copyright 2018 National Library of Norway
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package main
 
 import (
 	"os"
 
 	"github.com/ghodss/yaml"
-	log "github.com/sirupsen/logrus"
+	"github.com/pkg/errors"
 	"github.com/spf13/viper"
 	"golang.org/x/oauth2"
 )
@@ -18,20 +32,25 @@ type config struct {
 // Write config file
 //
 // Writes an oauth2 access token to config file
-func writeConfig() {
+func writeConfig() error {
 	token := viper.Get("token").(*oauth2.Token)
 	c := config{Token: token}
 
 	y, err := yaml.Marshal(c)
 	if err != nil {
-		log.Fatal(err)
+		errors.Wrap(err, "failed to marshal yaml")
 	}
 
 	f, err := os.Create(viper.ConfigFileUsed())
 	if err != nil {
-		log.WithError(err).Fatal()
+		return errors.Wrap(err, "failed to create config file")
 	}
 	defer f.Close()
+
 	f.Chmod(0600)
-	f.Write(y)
+	_, err = f.Write(y)
+	if err != nil {
+		return errors.Wrap(err, "failed to write config file")
+	}
+	return nil
 }
--- cmd/sigridrctl/search.go
+++ cmd/sigridrctl/search.go
@@ -1,7 +1,22 @@
+// Copyright 2018 National Library of Norway
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package main
 
 import (
 	"fmt"
+	"os"
 	"strings"
 	"time"
 
@@ -148,14 +163,18 @@ func initConfig() {
 			panic(err)
 		}
 		viper.Set("token", token)
-		writeConfig()
+		if err := writeConfig(); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+		}
 		return
 	}
 
 	// If access token provided, use it and store it in config file
 	if accessToken := viper.Get("access-token"); accessToken != "" {
 		viper.Set("token", &oauth2.Token{AccessToken: accessToken.(string)})
-		writeConfig()
+		if err := writeConfig(); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+		}
 		return
 	}
 }
